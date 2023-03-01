21d6c52077016d913fa94d41414721428183296a
AutomationBot <automation@example.com>
2023-03-01 17:56:47 -0500

    fix: adds permissions fixes for GH actions regenerate workflow
    Signed-off-by: AutomationBot <automation@example.com>

--- automation/lib/update.sh
+++ automation/lib/update.sh
@@ -2,6 +2,8 @@
 
 source ./automation/lib/logging.sh
 
+trestleDirs=("assessment-plans" "assessment-results" "catalogs" "component-definitions" "dist" "markdown" "plan-of-action-and-milestones" "profiles" "system-security-plans")
+
 export COMMIT_TITLE="chore: automatic content update"
 export COMMIT_BODY="Sync OSCAL content"
 git config --global user.email "automation@example.com"
@@ -11,12 +13,16 @@ if [ -z "$(git status --porcelain)" ]; then
   run_log 0 "Nothing to commit"
 else
   git diff
-  git add -A
+  for i in "${trestleDirs[@]}"
+  do
+    echo "Adding directory $i"
+  	git add "$i"
+  done
   if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
      run_log 0 "Nothing to commit"
   else
      git commit -m "$COMMIT_TITLE"
-     git push -u "autoupdate_$GITHUB_RUN_ID"
+     git push -u origin "autoupdate_$GITHUB_RUN_ID"
      run_log 0 "$COMMIT_BODY"
      gh pr create -t "$COMMIT_TITLE" -b "$COMMIT_BODY" -B "main" -H "autoupdate_$GITHUB_RUN_ID"
   fi
